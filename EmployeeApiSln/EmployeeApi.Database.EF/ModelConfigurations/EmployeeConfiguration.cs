c475450746575b957133fdabeae3f3e519085119
Gennadios <egon.sch@yandex.ru>
2022-10-31 18:03:51 +0400

    add employee model configuration

---
+++ EmployeeApiSln/EmployeeApi.Database.EF/ModelConfigurations/EmployeeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeApi.Database.EF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeApi.Database.EF.ModelConfigurations
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(employee => employee.FirstName)
+                .HasColumnName("first_name")
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.Property(employee => employee.LastName)
+                .HasColumnName("last_name")
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.Property(employee => employee.Position)
+                .HasColumnName("position")
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.Property(employee => employee.Salary)
+                .HasColumnName("salary")
+                .IsRequired();
+
+            builder.Property(employee => employee.JoinDate)
+                .HasColumnName("join_date")
+                .IsRequired();
+        }
+    }
+}
