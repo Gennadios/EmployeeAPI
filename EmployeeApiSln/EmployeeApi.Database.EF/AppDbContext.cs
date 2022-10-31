8e02111cde9152481e166f5cb3a73944fc97ecb5
Gennadios <egon.sch@yandex.ru>
2022-10-31 18:08:18 +0400

    add AppDbContext class

---
+++ EmployeeApiSln/EmployeeApi.Database.EF/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeApi.Database.EF.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace EmployeeApi.Database.EF
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
