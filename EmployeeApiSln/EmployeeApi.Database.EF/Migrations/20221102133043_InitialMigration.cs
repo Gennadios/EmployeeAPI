defe8c2610051701994573706d819a3bdd2f74e8
Gennadios <egon.sch@yandex.ru>
2022-11-02 17:35:19 +0400

    add db migration

---
+++ EmployeeApiSln/EmployeeApi.Database.EF/Migrations/20221102133043_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeApi.Database.EF.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    first_name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    position = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    salary = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    join_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
