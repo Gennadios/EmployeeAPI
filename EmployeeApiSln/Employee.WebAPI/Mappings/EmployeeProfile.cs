001f3bd98c0b37056efa20668251b25b8e12b1c4
Gennadios <egon.sch@yandex.ru>
2022-11-02 23:12:50 +0400

    add employee models mapping profile

---
+++ EmployeeApiSln/Employee.WebAPI/Mappings/EmployeeProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Employee.WebAPI.ApiModels.Employee;
+
+namespace Employee.WebAPI.Mappings
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            // Assuming that we add a new employee on their day 1.
+            var utcNow = DateTime.UtcNow;
+
+            CreateMap<CreateEmployeeApiModel, EmployeeApi.Database.EF.Models.Employee>()
+                .ForMember(x => x.JoinDate, x => x.MapFrom(x => utcNow));
+
+            CreateMap<EmployeeApi.Database.EF.Models.Employee, EmployeeApiModel>();
+        }
+    }
+}
