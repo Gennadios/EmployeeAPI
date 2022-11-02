913e0e45a268c2f5d03f38df320d4646275b4613
Gennadios <egon.sch@yandex.ru>
2022-11-02 18:07:29 +0400

    add employee api models

---
+++ EmployeeApiSln/Employee.WebAPI/ApiModels/Employee/CreateEmployeeApiModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee.WebAPI.ApiModels.Employee
+{
+    public class CreateEmployeeApiModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ EmployeeApiSln/Employee.WebAPI/ApiModels/Employee/EmployeeApiModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Employee.WebAPI.ApiModels.Employee
+{
+    public class EmployeeApiModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime JoinDate { get; set; }
+    }
+}
